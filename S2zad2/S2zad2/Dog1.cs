b1f59565af7880c203b6a58ce87358fe92c926bc
Piotr <34800836+Gulpin97@users.noreply.github.com>
2019-04-14 21:15:08 +0200

    Add files via upload

---
+++ S2zad2/S2zad2/Dog1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2zad2
+{
+    class Dog1
+    {
+        public int wiek;
+        public string rasa;
+        public double waga;
+
+        public void Szczekaj(string szczekanie) // metoda
+        {
+            Console.WriteLine(szczekanie);
+        }
+    }
+}
---
+++ S2zad2/S2zad2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzytkownik user1 = new Uzytkownik();
+            user1.wiek = 34;
+            user1.imie = "Kamil";
+
+            Uzytkownik user2 = new Uzytkownik();  // deklarowanie obiektu
+            user1.wiek = 10;
+            user1.imie = "Michał";
+
+            Dog1 dog = new Dog1();
+            Dog1 dogH = new Dog1();
+            dog.wiek = 5;
+            dog.rasa = "Pug";
+            dog.waga = 4.5f;
+            dog.Szczekaj("Pug szczeka");
+            dogH.Szczekaj("Huskey szczeka");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Uzytkownik // klasa
+    {
+        public int wiek;
+        public string imie;
+
+    }
+}
